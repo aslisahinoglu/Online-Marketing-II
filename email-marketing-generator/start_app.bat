91ed54a024fec2a2e0036512d1f48f85f7bf05a3
hannahlayher <hlayher@live.de>
2025-06-06 13:42:40 +0200

    Leere Dateien gelöscht

--- email-marketing-generator/start_app.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-call env\Scripts\activate.bat
-where streamlit
-streamlit run main.py
-pause
--- email-marketing-generator/start_app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-source env/bin/activate
-streamlit run main.py
